c3008486d04efcda63e0e8991ba43effffab234c
Richard <richardparado61@gmail.com>
2024-12-21 19:42:41 +0800

    adjust

--- src/components/SkillsCapsul.jsx
+++ src/components/SkillsCapsul.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import htmlImage from '../assets/html-5.png'
 function SkillsCapsul() {
   return (
-    <div className=' min-w-40 h-14 border-gray-800 flex items-center hover:flex-row-reverse duration-700 hover:m-1 justify-between rounded-xl border  ease-in-out hover:border-white
+    <div className='w-32 sm:w-40 h-14 border-gray-800 flex items-center hover:flex-row-reverse duration-700 hover:m-1 justify-between rounded-xl border  ease-in-out hover:border-white
    '>
         <div className='flex justify-center items-center h-full  bg-capsul w-20 rounded-xl'>
             <img src={htmlImage} alt="" className='w-10 h-auto m-4'/>
