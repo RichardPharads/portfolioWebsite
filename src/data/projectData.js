a12c9e5095d548b0e3a49ee1776569478259dfb3
RichardPharads <richardparado61@gmail.com>
2025-12-24 23:07:42 +0800

    remove mongoDB_pdf

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,7 +1,7 @@
 import freeboothImage from '../assets/freebooth.png'
 import arcadeThesis from '../assets/ArcadeThesis.png'
 import erpFrontend from '../assets/ERP.png'
-
+import dashboardPng from '../assets/dashboard.png'
 const projectDetails = [
     {   id:1,
         title: "FreeBooth",
@@ -30,7 +30,7 @@ const projectDetails = [
         title: "Arcangrovion Dashboard",
         detail: 'Artificial Intelligence (AI) - powered platform that provides insights and analytics for Robot.',
         date: 'July - Jan 2024',
-        image: erpFrontend
+        image: dashboardPng
 
     },
 ]
