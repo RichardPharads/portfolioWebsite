2a82d90e7f5bd8e7b10a776bd0e24076b47442ec
RichardPharads <richardparado61@gmail.com>
2025-12-25 10:47:08 +0800

    fix motion wrapper to y position bug fix horizontal overflow

--- src/components/MotionWrapper.jsx
+++ src/components/MotionWrapper.jsx
@@ -4,8 +4,8 @@ const MotionWrapper = ({ children }) => {
   return (
     <motion.div
       className="overflow-x-hidden"
-      initial={{ opacity: 0, x: 100 }}
-      whileInView={{ opacity: 1, x: 0 }}
+      initial={{ opacity: 0, y: 100 }}
+      whileInView={{ opacity: 1, y: 0 }}
       viewport={{ once: false, amount: 0.2 }}
       transition={{ duration: 0.5, ease: "easeInOut" }}
     >
